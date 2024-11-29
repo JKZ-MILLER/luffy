e946fc7bab339c0de2fcfed07b26a3a316c1a598
JKZ-MILLER <juancarlosdasilva2021@gmail.com>
2024-11-29 17:50:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Piratas',
+    'Quem é Luffy?',
+    'Luffy é o protagonista de One Piece, um pirata que sonha em se tornar o Rei dos Piratas e encontrar o tesouro One Piece. Ele é o capitão dos Chapéus de Palha.'
+);
+
+criaCartao(
+    'Habilidades',
+    'O que é o Gomu Gomu no Mi?',
+    'O Gomu Gomu no Mi é a Akuma no Mi que Luffy comeu, tornando seu corpo elástico como borracha. Isso lhe dá habilidades únicas em combate.'
+);
+
+criaCartao(
+    'Tripulação',
+    'Quem é o espadachim dos Chapéus de Palha?',
+    'O espadachim é Zoro Roronoa, um dos primeiros membros a entrar na tripulação. Ele é um mestre no estilo de três espadas.'
+);
+
+criaCartao(
+    'Mar e aventuras',
+    'O que é a Grand Line?',
+    'A Grand Line é uma rota perigosa no mar onde muitos piratas viajam em busca de aventuras e do lendário tesouro One Piece.'
+);
